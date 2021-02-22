854794854b6067c87273ebce116bbc8688c22599
mallmann02 <leomallmann020302@gmail.com>
2021-02-22 12:39:26 -0300

    Changing SSL param

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,7 @@ module.exports = {
   
   stagging: {
     client: 'pg',
-    connection: "postgres://nlvonlychcrcne:871fa8f063ebea1abc395e76191af2877f36bd6e59e051103f4b75ed17147dd1@ec2-54-198-73-79.compute-1.amazonaws.com:5432/davroh10d0mspk?ssl=true",
+    connection: "postgres://nlvonlychcrcne:871fa8f063ebea1abc395e76191af2877f36bd6e59e051103f4b75ed17147dd1@ec2-54-198-73-79.compute-1.amazonaws.com:5432/davroh10d0mspk?ssl=no-verify",
     ssl: {
       rejectUnauthorized: false
     },
